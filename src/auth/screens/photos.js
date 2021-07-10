d2f3425ffcd1dc3466423dbeb90b8123686acf2b
WaiswaDonnie <57533701+WaiswaDonnie@users.noreply.github.com>
2021-07-10 03:00:47 +0300

    Photos

--- src/auth/screens/photos.js
+++ src/auth/screens/photos.js
@@ -1,8 +1,10 @@
 
 
-import React,{useLayoutEffect} from 'react'
+import React,{useLayoutEffect,useState} from 'react'
 import {View,Text,FlatList,Image,StyleSheet,TouchableOpacity} from 'react-native';
 import MaterialIcons from "react-native-vector-icons/MaterialIcons";
+import Modal from 'react-native-modal'
+import ImageViewer from 'react-native-image-zoom-viewer';
 function Photos({navigation}) {
     useLayoutEffect(() => {
         navigation.setOptions({
@@ -34,80 +36,135 @@ function Photos({navigation}) {
               ),
         })
     },[navigation])
-    const columns =3
+    const columns =2
     const photos = [
       {
         key: Math.random(),
-        image: "https://unsplash.com/photos/yZlNNzVHsYg?utm_source=unsplash&utm_medium=referral&utm_content=creditShareLink",
+        url: "https://images.unsplash.com/photo-1511671782779-c97d3d27a1d4?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bGl2ZSUyMG11c2ljfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80",
       },
       {
         key: Math.random(),
-        image: "https://unsplash.com/photos/sDHrgXXCkpU",
+        url: "https://thumbs.dreamstime.com/b/environment-earth-day-hands-trees-growing-seedlings-bokeh-green-background-female-hand-holding-tree-nature-field-gra-130247647.jpg",
       },
       {
         key: Math.random(),
-        image: "https://unsplash.com/photos/fXQppSiTRGg",
+        url: "https://i0.wp.com/www.naijanowell.com/wp-content/uploads/2019/12/DFF.jpg?resize=300%2C300&ssl=1",
       },
       {
         key: Math.random(),
-        image: "https://unsplash.com/photos/9yfWx6GpzKg",
+        url: "https://i1.sndcdn.com/avatars-000314597584-5ebg5z-t500x500.jpg",
       },
       {
         key: Math.random(),
-        image: "https://unsplash.com/photos/9yfWx6GpzKg",
+        url: "https://images.unsplash.com/photo-1544785349-c4a5301826fd?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8ZGolMjBjb250cm9sbGVyfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80",
       },
       {
         key: Math.random(),
-        image: "https://unsplash.com/photos/9yfWx6GpzKg",
+        url: "https://media.istockphoto.com/photos/console-desk-at-nightclub-picture-id611607148?k=6&m=611607148&s=612x612&w=0&h=SCaZ9Ycx3AHmtluNn_vQf9K4yoREa0dnhXUXICciqZo=",
       },
       {
         key: Math.random(),
-        image: "https://unsplash.com/photos/9yfWx6GpzKg",
+        url: "https://www.popsci.com/uploads/2021/01/09/FUYQCMIJ25HXZKOQ576DID3M7E.jpg?width=1440",
       },
      
       
       {
         key: Math.random(),
-        image: "https://unsplash.com/photos/9yfWx6GpzKg",
+        url: "https://thumbs.dreamstime.com/b/friends-enjoying-house-party-friends-posing-selfie-wearing-dark-sunglasses-house-party-young-men-women-having-118513569.jpg",
       },
       {
         key: Math.random(),
-        image: "https://unsplash.com/photos/9yfWx6GpzKg",
+        url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTGrFLqPVMyKGyydENTwmTELVcDb4akkMYN0w&usqp=CAU",
       },
       {
         key: Math.random(),
-        image: "https://unsplash.com/photos/9yfWx6GpzKg",
+        url: "https://i.pinimg.com/originals/78/fb/3f/78fb3f5b04e5669483491751b08b6e29.jpg",
       },
       {
         key: Math.random(),
-        image: "https://unsplash.com/photos/9yfWx6GpzKg",
+        url: "https://smlnews.ug/wp-content/uploads/2020/09/images-11.jpeg",
       },
     
       
       
       {
         key: Math.random(),
-        image: "https://unsplash.com/photos/9yfWx6GpzKg",
+        url: "https://cached.imagescaler.hbpl.co.uk/resize/scaleWidth/815/cached.offlinehbpl.hbpl.co.uk/news/OMC/DESPERADOS-SMALL-APARTMENT-20190729094133314.jpg",
       },
   
       
       
     ];
+
+
+
+    const [visible,setVisible] =  useState(false)
+    const [imageIndex,setImageIndex] =  useState(null)
     
-    
+    function toggleModal() {
+
+      setVisible(!visible)      
+    }
     return (
         
    <View style={styles.container}>
     <FlatList 
     numColumns={columns}
     data={photos}
-    renderItem={({item})=>(
+    renderItem={({item,index})=>(
         // <Text>Sonnie</Text>
-        <TouchableOpacity>
-            <Image style={styles.image} source={require('../../img/photo.png')}/>
+        <TouchableOpacity onPress={()=>{
+          toggleModal()
+          setImageIndex(index)
+        }}>
+
+            <Image style={styles.image} source={{uri:item.url}}/>
         </TouchableOpacity>
     )}
     />
+     <Modal
+     style={{
+      margin: 0
+    }}
+
+          
+    animationIn="bounceInUp"
+    animationInTiming={900}
+ 
+ 
+     fullScreen visible={visible} transparent={true}>
+      
+                <ImageViewer imageUrls={photos}
+                 index={imageIndex} 
+                 enableImageZoom={true}
+                //  style={{width:500,height:350}}
+
+                renderHeader={()=>(
+                  <>
+                  <TouchableOpacity
+       onPress={toggleModal}
+       style={{
+        
+         zIndex:100,
+         justifyContent:'center',
+        alignItems:'flex-end',
+        paddingTop:30,
+        position:'absolute',
+        right:0
+       }}
+       > 
+
+     <MaterialIcons
+    
+     name="close-fullscreen" size={30} color='#fff' />
+       </TouchableOpacity>
+                  </>
+                )}
+                 />
+
+
+                 
+            </Modal>
 
     </View>
     )
@@ -121,12 +178,12 @@ const styles = StyleSheet.create({
         // ...StyleSheet.absoluteFillObject,
         justifyContent:'center',
         alignItems: 'center',
-        padding:15
+        // paddingHorizontal:15
     },
 image:{
     borderRadius:5,
-    height:102,
-    width:102,
+    height:202,
+    width:202,
 
     
     marginLeft:10,
